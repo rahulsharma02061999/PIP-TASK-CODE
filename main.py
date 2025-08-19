59a125ab73312dc5dbb304f9e016b9db4c233d33
rahulsharma02061999 <rahulsharma85746@gmail.com>
2025-08-19 22:33:11 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,18 +1,40 @@
-try:
-    a = int(input("Enter the first number: "))
-    b = int(input("Enter the second number: "))
-    print("What kind of operation you want to do.\n press + for addition \n press - for subtraction \n press * for multiplication \n press / for division")
-    choice = input("Enter your choice: ")
-    match choice:
-        case "+":
-            print(f"The sum of {a} and {b} is: {a+b}")
-        case "-":
-            print(f"fThe difference of {a} and {b} is: {a-b}")
-        case "*":
-            print(f"The multiplication of {a} and {b} is {a*b}")
-        case "/":
-            print(f"The division of {a} and {b} is {a/b}")
-        case default:
-           print("There was and error")
-except Exception as e:
-    print("Enter a valid Value")
+'''opening file in write mode'''
+#opening the information.txt file
+file = open("information.txt","w")
+#adding content to the file which will overwrite the previous data
+file.write("This is my to do task list:\n")
+file.write("1.I have to wake up early in the morning\n2.I have to go for running\n3.I have to learn python\n")
+#Closing the file
+file.close()
+
+'''Opening the file in read mode'''
+#Here I am opening a information.txt file 
+file = open("information.txt","r")
+#Reading the content of the file
+content = file.read()
+#printing the content
+print(content)
+#closing the file
+file.close()
+
+'''appending data to the file'''
+with open("information.txt","a") as file:
+    file.write("4.I have to send daily Status.\n")
+
+
+'''again opening the file to check wether data is added or not'''
+with open("information.txt","r") as file:
+    content = file.read()
+    print(content)
+
+'''adding many lines at the same time'''
+lines = ["5. I have to eat healthy\n", "6. I have to focous on my work"]
+with open("information.txt","a") as file:
+    file.writelines(lines)
+
+'''again opening the file to check wether data is added or not'''
+with open("information.txt", "r") as file:
+    print(file.read())
+
+'''This is how we can save notes to a file and also we can maintain a to do list and read it later'''
+
